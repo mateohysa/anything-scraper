aa7899c9286334ec9c26f13ccf17ae365229ca34
mateohysa <hysamateo@gmail.com>
2024-10-24 15:20:27 +0200

    script for scraping rally_rd asstes, usually with no login. might need some adjusting

--- source/scrape_rallyrd.py
+++ source/scrape_rallyrd.py
@@ -18,13 +18,13 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-chrome_options = ChromeOptions()
+"""chrome_options = ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument("--window-size=1920x1080")
 chrome_options.add_argument('--disable-gpu')
 userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36"
-chrome_options.add_argument(f'user-agent={userAgent}')
+chrome_options.add_argument(f'user-agent={userAgent}')"""
 
 def get_rally_data():
     for i in range(0,10):
