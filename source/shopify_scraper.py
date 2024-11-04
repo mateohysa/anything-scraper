2f4d22511ff4b981de5a61e0b5d7a48bff56c7e2
mateohysa <hysamateo@gmail.com>
2024-11-04 22:45:15 +0100

    basic scraper for shopify sites

---
+++ source/shopify_scraper.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
+PROXY = os.getenv("PROXY")
+proxies = {
+    "http": PROXY,
+    "https": PROXY
+}
+import requests
+import pandas as pd
+import string
+
+def scrape_bestsellers(site_url):
+    url = site_url+"/collections/bestsellers/products.json?limit=1000"
+    r = requests.get(url, proxies=proxies)
+    data = r.json()
+    return data["products"]
+
+if __name__ == "__main__":
+    site_url = "https://www.shopify.com/" #this can be the url of the shopify site
+    data = scrape_bestsellers(site_url)
+    df = pd.DataFrame(data)
+    df.to_csv("bestsellers.csv")
+
